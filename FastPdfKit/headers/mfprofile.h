4a4e825b4f06808063a3b46948d91d0844b37c04
nick <nick@mobfarm.eu>
2011-02-03 19:13:42 +0100

    Added profiling for the TJ operator.
    Signed-off-by: nick <nick@mobfarm.eu>

--- FastPdfKit/headers/mfprofile.h
+++ FastPdfKit/headers/mfprofile.h
@@ -14,6 +14,7 @@
 #define OP_TD 1
 #define OP_Tm 2
 #define OP_Tstar 3
+#define OP_TJ 4
 
 /*
  This is an experimental feature.
@@ -23,30 +24,32 @@
  
 typedef struct MFProfile {
 	
-	/// Td, TD, Tm, Tstar
+	/// Td, TD, Tm, Tstar, TJ
 	// 0 - do nothing
 	// 1 - add space with trimm
 	// 2 - add new line
 	
 	// Example:
 	// The array {1,2,1,0} will cause the Td operator to add a space with trim, TD to add a new line, Tm to act like Td, and Tstar to do nothing.
+	// TJ is only considered when the adjustment is greater than the span of the space.
 	
-	unsigned int fpdfk_xtr_policy[4];
+	unsigned int fpdfk_xtr_policy[5];
 	
-	// Td, TD, Tm, Tstar
+	// Td, TD, Tm, Tstar, TJ
 	// 0 - do nothing
 	// 1 - reset check
 	// 2 - reset check and add space
 	// 3 - add space
 	// Example:
 	// The array {0,1,3,2} will cause the search to just use the string passed after Td operator as a continuation of the string already checked, then to reset the check before continue on TD, to reset the check and add a space before taking in consideration the text passed after Tm, and just add a space when T* is encountered.
+	// TJ is only considered when the adjustment is greater than the span of the space.
 	
-	unsigned int fpdfk_src_policy[4];
+	unsigned int fpdfk_src_policy[5];
 	
 } MFProfile;
 
 void initProfile(MFProfile * profile);
-void initProfileWithSettings(MFProfile * profile, int xtr0, int xtr1, int xtr2, int xtr3, int src0, int src1, int src2, int src3);
+void initProfileWithSettings(MFProfile * profile, int xtr0, int xtr1, int xtr2, int xtr3, int xtr4, int src0, int src1, int src2, int src3, int src4);
 
 
 #endif
